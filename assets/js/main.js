cc9da1d1ef2a404899f15449da40252e9246d3a5
cindy <cindialy@gmail.com>
2017-07-12 13:24:58 -0400

    avance2 js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
 
+ function pokemones(data){
+     for(var data = 1; data < 20; data++){
+		console.log(data);
+		var lista= $('<li/>');
+		var botton = $('<button/>',{'class' : 'button-poke button-img'});
+		var span = $('<span/>'); 
+		var pokemones =  $('<img/>',{'src': 'https://pokeapi.co/media/img/' + data + '.png'});
 
+        lista.append(botton);
+        lista.append(span);
+        botton.append(pokemones);
+		$('.lista-pokemones').append(lista);
+	}
+}
   $.ajax({
   	url:  'http://pokeapi.co/api/v2/pokedex/',
   	type: 'GET',
